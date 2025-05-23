689b0cb0bfd1ad050f2bb47bc7c932dd4a33415c
danielcondorc <daniel.condor.c@upch.pe>
2025-05-22 20:46:32 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ from ramachandraw.utils import plot
 from io import BytesIO 
 
 st.title("Generador de Diagrama de Ramachandran")
-st.text("Autor: Jesús Alvarado-Huayhuaz")
+st.text("Autor: DCC")
 
 st.sidebar.image("ramachandran_logo.png", caption="inRamachandran")
 
@@ -31,5 +31,5 @@ st.download_button(
     mime="image/png"
 )
 
-st.balloons()
+st.star()
 
